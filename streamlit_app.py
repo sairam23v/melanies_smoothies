243c63e80b62e32d7b1ff149cec4764e19593947
sairam23v <77195189+sairam23v@users.noreply.github.com>
2024-06-03 19:41:33 +0530

    Update streamlit_app.py
    Correct version of the Smoothie Order App

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,9 @@ st.write("Choose the fruits you want in your custom Smoothie!")
 
 #st.write('Your favorite fruit is: ',option)
 
+name_on_order = st.text_input('Name on Smoothie: ')
+st.write('Name on the Smoothie will be: ',name_on_order)
+
 cnx = st.connection("snowflake")
 # session = get_active_session()
 session = cnx.session()
@@ -18,7 +21,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients'
-                                 ,my_dataframe)
+                                 ,my_dataframe
+                                 ,max_selections =5)
 
 if ingredients_list:
 #    st.write(ingredients_list)
@@ -31,10 +35,10 @@ if ingredients_list:
 
     # st.write(ingredients_string)
     
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-            values ('""" + ingredients_string + """')"""
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+            values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
-    # st.write(my_insert_stmt)
+#    st.write(my_insert_stmt)
 
     time_to_insert = st.button('Submit Order')
 
@@ -42,4 +46,4 @@ if ingredients_list:
     
         if ingredients_string:
             session.sql(my_insert_stmt).collect()
-            st.success('Your Smoothie is ordered!', icon="✅")
+            st.success('Your Smoothie is ordered '+ name_on_order+'!!', icon="✅")
